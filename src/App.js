1ac5044dd5b366c893ae5ffb65c51ee34d69f3a6
Sharad Sharma <119737825+sharmasharad311@users.noreply.github.com>
2023-06-08 21:26:23 +0530

    this is a calculator created using React.js.

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+import "./App.css";
+
+import Calculator from "./Elements/Calculator";
+
+function App() {
+  const [input, setInput] = useState("");
+ 
+  // Function for digit clicks
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  // function for evaluation
+  const handleEqual = () => {
+    setInput(eval(input).toString());
+    let result = Function("return " + input)();
+    //or
+    
+    // let result = eval(input).toString();
+    setInput(result);
+  };
+  const handleClear = () => {
+    setInput("");
+  };
+  return (
+    <div className="App">
+      <div className="calculator">
+        <input
+          type="text"
+          readOnly
+          className="display"
+          value={input}
+          placeholder="0"
+        />
+        <Calculator
+          handleClear={handleClear}
+          handleClick={handleClick}
+          handleEqual={handleEqual}
+        />
+      </div>
+    </div>
+  );
+}
+export default App;
